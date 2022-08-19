61e66e35f367c91115c0e938a35f8395abfc0fd2
tecc <tecc@tecc.me>
2022-08-19 19:01:39 +0200

    fix(tests): Add assertions for parsed values in nobody request parsing test

--- src/test/java/me/tecc/httputils/tests/requests/RequestParsingTest.java
+++ src/test/java/me/tecc/httputils/tests/requests/RequestParsingTest.java
@@ -2,18 +2,26 @@ package me.tecc.httputils.tests.requests;
 
 import me.tecc.httputils.request.HttpRequest;
 import me.tecc.httputils.request.HttpRequestParser;
+import me.tecc.httputils.utils.HttpMethod;
 import me.tecc.httputils.utils.HttpParseException;
+import me.tecc.httputils.utils.HttpVersion;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.StringReader;
 import static java.nio.charset.StandardCharsets.US_ASCII;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RequestParsingTest {
     @Test
     public void noBody() throws IOException, HttpParseException {
         HttpRequestParser parser = new HttpRequestParser(new ByteArrayInputStream("GET /example/path HTTP/1.1\r\n\r\n".getBytes(US_ASCII)));
         HttpRequest request = parser.parse();
+
+        assertEquals(HttpMethod.GET, request.getMethod());
+        assertEquals(HttpVersion.V1_1, request.getVersion());
+        assertEquals("/example/path", request.getPath());
+        assertEquals(0, request.getHeaders().count());
+        assertEquals(0, request.getBody().limit());
     }
 }
